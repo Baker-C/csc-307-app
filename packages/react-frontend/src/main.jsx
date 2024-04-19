f480e4eccfa6cc52041de0069a2eadb30e5b4cf5
Charles Baker <charlesbaker.bakersbinary.com>
2024-04-18 20:16:34 -0700

    IE1

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MyApp from "./MyApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOM.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);
